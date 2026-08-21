1ccfb9d9df1bef7812c1213c9c432ebf408c489b
neerajsutha973r <neerajsuthar227@gmail.com>
2026-08-21 11:18:48 +0530

    Time: 11 ms (8.04%), Space: 16.3 MB (9.19%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*,TreeNode*>parent;
+    void preorder(TreeNode* curr,TreeNode* par){
+        if(!curr) return;
+        parent[curr]=par;
+        preorder(curr->left,curr);
+        preorder(curr->right,curr);
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+       
+        preorder(root,NULL);
+        queue<TreeNode*>q;
+        q.push(target);
+        unordered_set<TreeNode*>visited;
+        while(!q.empty() and k){
+            k--;
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                TreeNode* curr=q.front();
+                q.pop();
+                visited.insert(curr);
+                if(!visited.count(curr->left) and curr->left) q.push(curr->left);
+                if(!visited.count(curr->right) and curr->right) q.push(curr->right);
+                if(!visited.count(parent[curr]) and parent[curr]) q.push(parent[curr]);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            TreeNode* node=q.front();
+            ans.push_back(node->val);
+            q.pop();
+        }
+        return ans;
+    }
+};
