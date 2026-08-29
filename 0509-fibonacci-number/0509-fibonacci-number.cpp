70bab76def9915dd71dad7a2485b3f87842e7496
NEERAJ SUTHAR <neerajsuthar227@gmail.com>
2026-08-29 21:15:50 +0530

    Time: 2 ms (77.15%), Space: 7.6 MB (98.9%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0;
+        int b=1;
+        if(n==0){
+            return a;
+        }
+        if(n==1){
+            return b;
+        }
+        int c=0;
+        for(int i=2;i<=n;i++){
+            c=a+b;
+            a=b;
+            b=c;
+}
+return c;
+        
+        
+    }
+};
